1a4886ca9aad6b2bc384d6aeddcab257a8386628
hajaeryul <wofbf2@gmail.com>
2022-09-30 17:56:34 +0900

    스투시 클론코딩 account header main

---
+++ src/main/java/com/stussy/stussyclone20220930jaeryul/StussyClone20220930JaeryulApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930jaeryul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20220930JaeryulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClone20220930JaeryulApplication.class, args);
+	}
+
+}
